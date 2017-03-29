4cabded71da5c933ced0e9ea2c9bdb7da9fb46fc
Naeno <daakhan@gmail.com>
2017-03-29 18:27:59 +0200

    basic data parsing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by Arnaud on 29/03/2017.
  */
+$.get('data/data.csv',function(data){
+    var dataStr = new String(data);
+    $.csv.toArrays(dataStr).forEach(function (element) {
+        $('#liste').append('<li>' + element[0] + ' : ' + element[1] + '</li>');
+    })
+},dataType='text');
